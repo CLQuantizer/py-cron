dd8700db0ff8980c3a57b3ce4a7fd462f91e6c43
ezio <ezioxxk@gmail.com>
2024-06-27 22:52:51 +0100

    go

--- db.py
+++ db.py
@@ -1,3 +1,5 @@
+from psycopg2 import OperationalError
+import time
 from dotenv import dotenv_values
 import psycopg2
 
@@ -8,7 +10,24 @@ connection = psycopg2.connect(PG_URL)
 
 
 def get_words_today():
-    cursor = connection.cursor()
-    cursor.execute("SELECT word FROM words WHERE updated_at::date = CURRENT_DATE")
-    words = cursor.fetchall()
-    return [word[0] for word in words]
+    retries = 3
+    while retries > 0:
+        try:
+            connection = psycopg2.connect(PG_URL)
+            cursor = connection.cursor()
+            cursor.execute("SELECT word FROM words WHERE updated_at::date = CURRENT_DATE")
+            words = cursor.fetchall()
+            cursor.close()
+            connection.close()
+            return [word[0] for word in words]
+        except OperationalError as e:
+            print(f"OperationalError: {e}")
+            retries -= 1
+            if retries > 0:
+                print(f"Retrying... {retries} attempts left.")
+                time.sleep(5)
+            else:
+                raise Exception("Failed to connect to the database after several attempts")
+            
+if __name__ == '__main__':
+    print(get_words_today())
