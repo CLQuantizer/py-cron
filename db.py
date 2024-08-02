74cf24b38a5fefe04bf874333085fe4068ec27f9
eziox <ezioxxk@gmail.com>
2024-08-02 22:39:42 +0200

    use alchemy

--- db.py
+++ db.py
@@ -1,33 +1,37 @@
+from datetime import datetime, timedelta
+
+from sqlalchemy import create_engine, Column, String, DateTime
+from sqlalchemy.orm import sessionmaker, declarative_base
 from psycopg2 import OperationalError
 import time
 from dotenv import dotenv_values
 import psycopg2
 
 config = dotenv_values(".env")
-PG_URL = config['PG']
+PG_URL = 'postgresql+psycopg2://' + config['PG'].split('://')[1]
 
-connection = psycopg2.connect(PG_URL)
+engine = create_engine(PG_URL)
+Base = declarative_base()
+
+
+# Define Word model
+class Word(Base):
+    __tablename__ = 'words'
+
+    word = Column(String, primary_key=True)
+    updated_at = Column(DateTime)
+
+
+# Create session factory
+Session = sessionmaker(bind=engine)
 
 
 def get_words_today():
-    retries = 3
-    while retries > 0:
-        try:
-            connection = psycopg2.connect(PG_URL)
-            cursor = connection.cursor()
-            cursor.execute("SELECT word FROM words WHERE updated_at >= NOW() - INTERVAL '24 hours'")
-            words = cursor.fetchall()
-            cursor.close()
-            connection.close()
-            return [word[0] for word in words]
-        except OperationalError as e:
-            print(f"OperationalError: {e}")
-            retries -= 1
-            if retries > 0:
-                print(f"Retrying... {retries} attempts left.")
-                time.sleep(5)
-            else:
-                raise Exception("Failed to connect to the database after several attempts")
-            
+    with Session() as session:
+        twenty_four_hours_ago = datetime.now() - timedelta(hours=24)
+        words = session.query(Word.word).filter(Word.updated_at >= twenty_four_hours_ago).all()
+        return [word[0] for word in words]
+
+
 if __name__ == '__main__':
     print(get_words_today())
